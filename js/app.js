6b59cc9e6e377e1fda6b01674bcfe77520ce1045
Neven Hajder <neven.hajder@gmail.com>
2014-12-27 21:31:17 +0900

    Flip card

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
-
-/* Create a lightbox for card flip */
-
-
 /* Add class fa-spin to #flip when user hovers */
 $('#flip').hover(
 	function() {
-		console.log("You're hovering.");
 		$('#flip i').addClass('fa-spin');
 	},
 	function() {
@@ -14,35 +9,58 @@ $('#flip').hover(
 );
 
 /* Variables for carousel manipulation */
-var carousel = $('.carousel-wrap');
-/* Translate and rotate variables */
-var tranZ = 277;
-var rotY = 0;
+var carousel = $('.carousel-wrap'),
+selectedCard = $('.selected > .inner-card-wrap'),
+tranZ        = 277,
+rotY         = 0;
+
+
+/* Update .selected */
+var selectNext = function() {
+	var $next = $('.selected').removeClass('selected').next('.card-wrap');
+
+	if ($next.length) {
+		$next.addClass('selected');
+	} else {
+		$('.card-wrap:first-child').addClass('selected');
+	}
+
+	selectedCard = $('.selected > .inner-card-wrap');};
+var selectPrev = function() {
+	var $prev = $('.selected').removeClass('selected').prev('.card-wrap');
+
+	if ($prev.length) {
+		$prev.addClass('selected');
+	} else {
+		$('.card-wrap:last-child').addClass('selected');
+	}
+
+	selectedCard = $('.selected > .inner-card-wrap');};
+
 
 /* Rotate carousel on #next.click */
 $('#next').on('click',
 	function() {
 		rotY -= 60;
 		carousel.css('-webkit-transform', 'translateZ(-' + tranZ + 'px) rotateY(' + rotY + 'deg)');
-		console.log('tranZ: ' + tranZ);
-		console.log('rotY: ' + rotY);
-});
 
+		selectNext();});
 
 /* Rotate carousel on #previous.click */
 $('#previous').on('click',
 	function() {
 		rotY += 60;
-		/* Fails when rotY goes below 0 */
 		carousel.css('-webkit-transform', 'translateZ(-' + tranZ + 'px) rotateY(' + rotY + 'deg)');
 
-		console.log('tranZ: ' + tranZ);
-		console.log('rotY: ' + rotY);
-});
+		selectPrev();});
 
 
 /* Flip the card */
 $('#flip').on('click',
 	function() {
-		/*Flip the card*/
+		if ( selectedCard.hasClass('flipped') ) {
+			selectedCard.css('-webkit-transform', 'rotateY(0)').toggleClass('flipped');
+		} else {
+			selectedCard.css('-webkit-transform', 'rotateY(180deg)').toggleClass('flipped');
+		}
 });
